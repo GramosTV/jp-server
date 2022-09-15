7a2779f037c548acb9c0a60e4b1fa6eb55a58028
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-09-15 22:54:21 +0200

    Authentication

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
+import { User } from './entity/user.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async findOne(email: string): Promise<User | undefined> {
+    return await User.findOneBy({ email });
+  }
+}
