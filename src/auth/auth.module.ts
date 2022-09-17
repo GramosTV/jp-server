b80b8e23f7f8ccaa8cef0c824b9b7123c2b4334f
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-09-17 16:33:44 +0200

    Changed jwt expire time to test value

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '15m' },
+      signOptions: { expiresIn: '999d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
