fa74e4668fec8ee50bde015ccd8a9f00d1efd2af
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-09-21 13:52:10 +0200

    finishDay endpoint in progress

--- src/auth/statsSet.decorator.ts
+++ src/auth/statsSet.decorator.ts
@@ -1,9 +1,14 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ConflictException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class statsSetGuard implements CanActivate {
+export class StatsSetGuard implements CanActivate {
   constructor(
     private reflector: Reflector,
     private usersService: UsersService,
@@ -12,6 +17,9 @@ export class statsSetGuard implements CanActivate {
   async canActivate(ctx: ExecutionContext) {
     const request = ctx.switchToHttp().getRequest();
     const { statsSet } = await this.usersService.findOneById(request.user.id);
+    if (!statsSet) {
+      throw new ConflictException();
+    }
     return !!statsSet;
   }
 }
--- src/planks/planks.service.ts
+++ src/planks/planks.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Day } from 'src/days/entity/day.entity';
+import { Repository } from 'typeorm';
+import { Plank } from './entity/plank.entity';
 
 @Injectable()
-export class PlanksService {}
+export class PlanksService {
+  constructor(
+    @InjectRepository(Plank) private dayRepository: Repository<Plank>,
+  ) {}
+
+  async getMany(id: string, plankNumber: number, dayId = '') {
+    if (!dayId) {
+      return await Plank.find({
+        where: { day: { user: { id } } },
+      });
+    }
+  }
+}
