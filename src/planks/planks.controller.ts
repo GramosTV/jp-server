c984fe622f5e10201eb8acc3bf433dda52a945b2
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-10-01 19:59:27 +0200

    Added delete plank endpoint

--- src/planks/planks.controller.ts
+++ src/planks/planks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { StatsSetGuard } from 'src/auth/statsSet.decorator';
+import { ParseIntMinMaxPipe } from 'src/pipes/parseIntMinMax.pipe';
+import { PlanksService } from './planks.service';
 
 @Controller('planks')
-export class PlanksController {}
+export class PlanksController {
+  constructor(private planksService: PlanksService) {}
+
+  @UseGuards(JwtAuthGuard, StatsSetGuard)
+  @Delete('/:numeration')
+  async findMany(
+    @Request() req,
+    @Param('numeration', new ParseIntMinMaxPipe(1, 65535)) numeration: number,
+  ) {
+    return this.planksService.deleteOne(req.user.id, numeration);
+  }
+}
