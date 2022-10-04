0f070c3ff5dfe9c887a61c65064756ee706f9e0f
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-10-04 21:36:14 +0200

    Added find friends endpoint

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { StatsSetGuard } from 'src/auth/statsSet.decorator';
+import { FriendsService } from './friends.service';
 
 @Controller('friends')
-export class FriendsController {}
+export class FriendsController {
+  constructor(private friendsService: FriendsService) {}
+
+  @UseGuards(JwtAuthGuard, StatsSetGuard)
+  @Get('/')
+  async findMany(@Request() req) {
+    return await this.friendsService.findMany(req.user.id);
+  }
+}
--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Bool } from 'types';
+import { Friend } from './entity/friend.entity';
 
 @Injectable()
-export class FriendsService {}
+export class FriendsService {
+  constructor(
+    @InjectRepository(Friend) private friendRepository: Repository<Friend>,
+  ) {}
+
+  async findMany(id: string) {
+    return await Friend.find({
+      where: [
+        { friendReceived: { id }, accepted: Bool.true },
+        { friendInvited: { id }, accepted: Bool.true },
+      ],
+    });
+  }
+}
