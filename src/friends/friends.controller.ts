caf866466cc7e33e908e7c6807bffa84d28fe97c
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-10-10 23:12:32 +0200

    Style change

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Patch, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { StatsSetGuard } from 'src/auth/statsSet.decorator';
 import { ParseStringMinMaxPipe } from 'src/pipes/parseStringMinMax.pipe';
