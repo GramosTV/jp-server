c3a82970862d041f17b47398920cc57be50195ff
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-10-14 13:03:32 +0200

    Added changeUnit and changeHeight endpoints

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { User } from './entity/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Units } from 'types';
 
 @Injectable()
 export class UsersService {
@@ -10,5 +11,17 @@ export class UsersService {
   ) {}
   async findOne(email: string): Promise<User | undefined> {
     return await User.findOneBy({ email });
+  }
+
+  async findOneById(id: string): Promise<User | undefined> {
+    return await User.findOneBy({ id });
+  }
+
+  async changeUnit(id: string, unit: Units) {
+    return (await User.update(id, { unit })).affected;
+  }
+
+  async changeHeight(id: string, height: number) {
+    return (await User.update(id, { height })).affected;
   }
 }
